86c3e16e6e321d178b3826960965ff2118f1c2c8
Débora Moyano <57150176+Debora36@users.noreply.github.com>
2024-04-04 11:30:06 -0300

    Add files via upload

---
+++ src/tp/pkg4/lab/TP4Lab.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp.pkg4.lab;
+
+public class TP4Lab {
+
+    public static void main(String[] args) {
+
+    }
+
+}
